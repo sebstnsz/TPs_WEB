187bc7440eb85017dd11fde7570028f7dc2f50b7
Alexandra Robin <ra401651@etu.unice.fr>
2018-12-04 15:34:24 +0100

    Modification vue Pagination
    Ajout des composants graphiques dans la vue Pagination, des méthodes associées, et ajout des importations des composants dans main.js

--- guide-des-resto/src/components/Pagination.vue
+++ guide-des-resto/src/components/Pagination.vue
@@ -1,6 +1,55 @@
 <template>
 	<div id="pagination">
-		<p>Pagination</p>
+		<el-row type="flex" justify="center">
+			<el-col>
+				<el-button-group>
+					<el-button  
+						icon="el-icon-d-arrow-left"
+						size="small"
+						v-on:click="premierePage"
+						v-bind:disabled="page<=0"
+						circle></el-button>
+					<el-button 
+						icon="el-icon-arrow-left"
+						size="small"
+						v-on:click="pagePrecedente"
+						v-bind:disabled="page<=0"
+						circle></el-button>
+				</el-button-group>
+			</el-col>
+			<el-col>
+				<el-input-number 
+					size="small"
+					:min="0"
+					:max="nbPagesMax"
+					v-model="page"
+					v-on:input="getRestaurantsFromServer">
+				</el-input-number>
+			</el-col>
+			<el-col>
+				<el-button-group>
+					<el-button 
+						icon="el-icon-arrow-right"
+						size="small"
+						v-on:click="pageSuivante"
+						v-bind:disabled="page>=nbPagesMax"
+						circle></el-button>
+					<el-button 
+						icon="el-icon-d-arrow-right"
+						size="small"
+						v-on:click="dernierePage"
+						v-bind:disabled="page>=nbPagesMax"
+						circle></el-button>
+				</el-button-group>
+			</el-col>
+		</el-row>
+		<!--<el-pagination
+			layout="prev, pager, next"
+			:total="1000"
+			:page-size="10"
+			background
+			small>
+		</el-pagination>-->
 	</div>
 </template>
 
@@ -10,12 +59,45 @@ export default {
 
 	data() {
 		return {
+			page: 0,
+			nbPagesMax: 10
 		}
 	},
 	components: { 	// LOCAL COMPONENTS
+	},
+	methods: {
+		getRestaurantsFromServer(){
+			console.log("Get restaurants");
+		},
+		pagePrecedente(){
+			if(this.page > 0){
+				this.page--;
+				this.getRestaurantsFromServer();
+			}
+		},
+
+		pageSuivante(){
+			if(this.page < this.nbPagesMax){
+				this.page++;
+				this.getRestaurantsFromServer();
+			}
+		},
+
+		premierePage(){
+			this.page = 0;
+			this.getRestaurantsFromServer();
+		},
+
+		dernierePage(){
+			this.page = this.nbPagesMax;
+			this.getRestaurantsFromServer();
+		}
 	}
 }
 </script>
 
 <style>
+el-input {
+	text-align: center;
+}
 </style>
--- guide-des-resto/src/main.js
+++ guide-des-resto/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
-import { Button, Select, Input, Icon } from 'element-ui';
+import { Button, Input, InputNumber, Icon, ButtonGroup, Row, Col, Pagination } from 'element-ui';
 import App from './App.vue';
 
-Vue.component(Button.name, Button);
-Vue.component(Input.name, Input);
-Vue.component(Icon.name, Icon);
+Vue.use(Button);
+Vue.use(Input);
+Vue.use(InputNumber);
+Vue.use(Icon);
+Vue.use(ButtonGroup);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Pagination);
 
 new Vue({
   el: '#app',
