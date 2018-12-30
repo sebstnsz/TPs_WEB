a4c839ff08d8eec3abf4a419d9dfc2d665ce2b0e
Sébastien <sebsaez1007@hotmail.fr>
2018-12-30 17:23:33 +0100

    <0007> Génération de la carte

---
+++ guide-des-resto/src/data.js
@@ -0,0 +1,71 @@
+entrees = [
+    {
+        nom : "Nachos",
+        description : "Chips, Cheddar, Bacon, Piments",
+        prix : 7.50
+    },{
+        nom : "Salade de chevre chaud",
+        description : "Jeunes pouces, Fromage de chevre",
+        prix : 3.50
+    },{
+        nom : "Mozza sticks x6",
+        description : "Mozzarela panné",
+        prix : 2.00
+    },{
+        nom : "Assiette de charcuterie",
+        description : "Jambon, Bacon, Mortadelle, Saucisson",
+        prix : 8.00
+    }
+];
+
+
+plats = [
+    {
+        nom : "Pizza Reine",
+        description : "Sauce tomate, Champignons, Jambon, Emmental",
+        prix : 12.50
+    },
+    {
+        nom : "Cote de boeuf",
+        description : "Accompagné de ses frites maison",
+        prix : 19.50
+    },
+    {
+        nom : "Magret de canard",
+        description : "Accompagné de tapenade olive noir et de ses frites maison",
+        prix : 15.50
+    },
+    {
+        nom : "Fish N Chips",
+        description : "Cabillaud et frites",
+        prix : 9.50
+    },
+    {
+        nom : "Burger montagnard",
+        description : "Steak, Galette de pomme de terre, Bacon, Frommage a raclette",
+        prix : 13.50
+    },
+];
+
+desserts = [
+    {
+        nom : "Fondant au chocolat",
+        description : "Fondant au chocolat fait maison par notre patissier",
+        prix : 4.50
+    },
+    {
+        nom : "Glace 2 boules",
+        description : "Poire, Chocolat, Citron, Vanille, Noix de coco, Raisin",
+        prix : 3.00
+    },
+    {
+        nom : "Tarte au citron",
+        description : "Tarte au citron meringuée",
+        prix : 4.50
+    },
+    {
+        nom : "Salade de fruit",
+        description : "Peche, Kiwi, Ananas, Raisin, Pomme",
+        prix : 3.50
+    },
+]
